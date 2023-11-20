c29a01b90e71902dbfbdbaf18ff554c0dd6ae57a
Safiya Amin <85015875+cypher-1802@users.noreply.github.com>
2023-11-20 14:19:20 +0530

    local IP upated

--- scripts/common.sh
+++ scripts/common.sh
@@ -78,7 +78,7 @@ sudo apt upgrade -y
 
 # local_ip="$(ip --json addr show eth0 | jq -r '.[0].addr_info[] | select(.family == "inet") | .local')"
 # echo "Local IP Address : $local_ip"
-local_ip="$(echo ifconfig.me)"
+local_ip="$(curl ifconfig.me)"
 sudo sh -c "cat > /etc/default/kubelet << EOF
 KUBELET_EXTRA_ARGS=--node-ip=$local_ip
 EOF"
