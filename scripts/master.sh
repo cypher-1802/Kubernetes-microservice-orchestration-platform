3ff3ec4910ae7ff6f67382aa7451e93c75722acf
Safiya Amin <85015875+cypher-1802@users.noreply.github.com>
2024-01-24 13:05:08 +0530

    Update master.sh

--- scripts/master.sh
+++ scripts/master.sh
@@ -39,7 +39,7 @@ sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
 
 # Install Calico Network Plugin Network 
 
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
+#kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
 
 #curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/custom-resources.yaml -O
 
